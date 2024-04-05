47c12cd256c808e6e933fc19c3f91a61a8084b8c
unknown <abrordevaxmedov@gmail.com>
2024-04-05 22:42:28 +0300

    first-commit

---
+++ js/about-page.js
@@ -0,0 +1,8 @@
+document.addEventListener("click", (e) => {
+  if (!e.target.matches(".header__menu-btn")) return;
+  e.target.classList.toggle("change-icon");
+  let navbar = e.target.nextElementSibling;
+  document.body.classList.toggle("unscroll");
+  navbar.classList.toggle("hide");
+});
+  
